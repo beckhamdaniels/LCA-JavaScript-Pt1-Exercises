b2a7b5c059c6980a24a2e6db1e2867a8272f5d17
Beckham Daniels <beckhamdaniels@icloud.com>
2026-05-19 01:18:59 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,17 @@
+let number1 = 10; //Variable called number1, assigned the value of 10//
+
+let number2 = 5; //Variable called number2, assigned the value of 5//
+
+let stringNumber = String(number1); //Variable called stringNumber, converting number1 to a string//
+
+let sum = number1 + number2; //Variable called sum, adding number1 and number2//
+
+let product = number1 * number2; //Variable called product, multiplying number1 and number2//
+
+console.log(`Number 1 is ${number1} and Number 2 is ${number2}.`); //Printing the values of number1 and number2//
+
+console.log(`The Number 1 as a string is ${stringNumber}.`); //Printing the string version of number1//
+
+console.log(`The sum of ${number1} and ${number2} is ${sum}.`); //Printing the sum//
+
+console.log(`The product of ${number1} and ${number2} is ${product}.`); //Printing the product//
