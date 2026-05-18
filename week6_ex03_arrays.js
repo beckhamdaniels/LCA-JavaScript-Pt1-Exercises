46333e392cd7ec9cc026f45c6214057dd81c254e
Beckham Daniels <beckhamdaniels@icloud.com>
2026-05-19 01:39:03 +0200

    Add Exercise 03 - Array Basics

---
+++ week6_ex03_arrays.js
@@ -0,0 +1,13 @@
+let colors = [] //Created an empty array called colors//
+
+colors.push("red")  // Added 3 new colours using push method//
+colors.push("green")
+colors.push("blue")
+
+let numbers = [1, 2, 3, 4, 5] //created a new array with 5 numbers//
+
+colors.pop() //Removed the last element of the colors array using pop method//
+
+colors.unshift("yellow") //Added a new colour to the beginning of the colors array using unshift method//
+
+console.log(`The length of colors is: ${colors.length}, The length of numbers is: ${numbers.length}`) //Printed the length of both arrays using length property//
