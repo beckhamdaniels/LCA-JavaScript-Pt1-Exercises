1363b04e57a492c4f7ffc163a72e761044ab9625
Beckham Daniels <beckhamdaniels@icloud.com>
2026-05-19 01:01:19 +0200

    Add Exercise 01 - Variables and String Manipulation

---
+++ week6_ex01_variables.js
@@ -0,0 +1,11 @@
+const firstName = "Beckham" //Variable called firstName, assigned the value "Beckham"//
+
+const lastName = "Daniels"; //Variable called lastName, assigned the value "Daniels"//
+
+let age = 21; //Variable called age, assigned the value of 21//
+
+age = 23; // Changing the value of age to 23//
+
+const fullName = firstName + " " + lastName; //Variable called fullName, combining firstName and lastName//
+
+console.log(`Welcome ${fullName}, you are ${age} years old!`); //Printing the greeting//
