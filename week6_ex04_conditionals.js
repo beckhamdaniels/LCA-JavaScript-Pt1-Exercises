194799681c8c4722d95beb461b5a560fc46e918a
Beckham Daniels <beckhamdaniels@icloud.com>
2026-05-19 02:05:28 +0200

    Add Exercise 04 - Conditional Logic

---
+++ week6_ex04_conditionals.js
@@ -0,0 +1,14 @@
+let score = 73; //Variable to hold score//
+
+const passingGrade = 60; //Variable to hold pass grade//
+
+if (score === 100){
+    console.log("Perfect score"); //conditon to check if score is 100//
+  }
+
+else if (score >= passingGrade) {  //else if statement to check if score is greater than or equal to pass mark//
+   console.log("Passed");
+}
+else {
+  console.log("Failed"); //else if score is less than pass mark//
+}
